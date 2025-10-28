1c1a330a9eec846c801f771542c4f2c88d0351e5
Michel Sanner <sanner@scripps.edu>
2025-10-28 09:08:59 -0700

    added license stuff

--- canonicalAA.c
+++ canonicalAA.c
@@ -1,3 +1,22 @@
+/*********************************************************************/
+/* AutoDock CrankPep, a peptide docking engine                       */
+/* Copyright (C) 2019 MICHEL SANNER                                  */
+/*                                                                   */ 
+/* This library is free software; you can redistribute it and/or     */
+/* modify it under the terms of the GNU Library General Public       */
+/* License as published by the Free Software Foundation; either      */
+/* version 2 of the License, or (at your option) any later version.  */
+/*                                                                   */ 
+/* This library is distributed in the hope that it will be useful,   */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of    */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU */
+/* Library General Public License for more details.                  */
+/*                                                                   */
+/* You should have received a copy of the GNU Library General Public */
+/* License along with this library; if not, see                      */
+/* <https://www.gnu.org/licenses/>.                                  */
+/*********************************************************************/
+
 /* structures defining canonical amino acids                  */
 /* CA is at origin, N on the -x axis, and CB in the z=0 plane */
 /*                                                            */
